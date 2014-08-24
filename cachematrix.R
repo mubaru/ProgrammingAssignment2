121600e4c4d63f202c71411e5e30e3e7c356ceb2
Kristof <kristof.alfoldi@gmail.com>
2014-08-24 22:59:40 +0200

    Sharing homework

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,30 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function lists both the given matrix and it's inverse.
 
+makeCacheMatrix <- function(x) {
+  y <- solve(x)
+  return(list(original = x, solved =y))
+  
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function checks, if the given list contains the inverse matrix
+## beside the original matrix. If it is there then this function lists 
+## both of them, if the solved one is missing then it calculates it.
+
+cachesolve <- function(x) {
+  n <- x$solved
+  if(!is.null(n)) {
+    message("getting cached data")
+    return(n)
+  }
+  data <- x$original
+  
+  x$solved <- solve(data)
+  return(x)
 }
+
